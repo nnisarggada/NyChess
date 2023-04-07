2731dd3428af7c1378f36d0fcdb5f6d0b444b720
Nnisarg Gada <gadannisarg@gmail.com>
2023-04-07 17:41:51 +0530

    yes

--- NyChessEnv/bin/activate.fish
+++ NyChessEnv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/run/media/nnisarggada/College/Nychess-APK/NyChessEnv"
+set -gx VIRTUAL_ENV "/home/nnisarggada/GitRepos/NyChess/NyChessEnv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
